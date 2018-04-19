eccaa1f683684169c9a20403a8f1e7b852500a1d
Kathy Girim Chung <kathychung@Kathys-MacBook.wv.cc.cmu.edu>
2018-04-19 02:11:50 -0400

    created function model_select that runs regression of responses on covariates returning covariates with p-values less than or equal to cutoff

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -4,3 +4,14 @@ generate_data = function(n,p){
   list(covariates, responses)
 }
 
+model_select = function(covariates, responses, cutoff){
+  retained = summary(lm(responses~covariates))$coefficients[,4][summary(lm(responses~covariates))$coefficients[,4] <= cutoff]
+  
+  indices = which(summary(lm(responses~covariates))$coefficients[,4][summary(lm(responses~covariates))$coefficients[,4] <= cutoff] %in% summary(lm(responses~covariates))$coefficients[,4])
+  
+  again = summary(lm(responses~covariates[,indices]))$coefficients[,4][summary(lm(responses~covariates[,indices]))$coefficients[,4] <= cutoff]
+  
+  if (length(again == 0)){
+    return(c())
+  }
+}
