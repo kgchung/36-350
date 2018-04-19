30f25a1bcf755449847b540345d882c140fcd6e6
Kathy Girim Chung <kathychung@Kathys-MacBook.wv.cc.cmu.edu>
2018-04-19 02:23:48 -0400

    function that runs regression of responses on covariates to return those less than or equal to cutoff

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -9,3 +9,10 @@ generate_data = function(n,p){
   list(covariates, responses)
 }.patch
 >>>>>>> b2c8d5debbca443cb3ce54a6bc3b60e6d696801e
+model_select = function(covariates, responses, cutoff){
+  retained = summary(lm(responses~covariates))$coefficients[,4][summary(lm(responses~covariates))$coefficients[,4] <= cutoff]
+  indices = which(summary(lm(responses~covariates))$coefficients[,4][summary(lm(responses~covariates))$coefficients[,4] <= cutoff] %in% summary(lm(responses~covariates))$coefficients[,4])
+  again = summary(lm(responses~covariates[,indices]))$coefficients[,4][summary(lm(responses~covariates[,indices]))$coefficients[,4] <= cutoff]
+  if (length(again == 0)){
+   return(c())}
+    }
