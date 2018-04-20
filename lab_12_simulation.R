38d98892dcb26b91f65270ffc07ef39ec18a7519
kgchung <38510598+kgchung@users.noreply.github.com>
2018-04-20 06:45:56 -0400

    Update lab_12_simulation.R

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -2,11 +2,4 @@ generate_data = function(n,p){
   covariates = matrix(rnorm(n*p,mean=0,sd=1), n, p) 
   responses = rnorm(n, 0, 1)                     
   list(covariates, responses)                    
-}
-model_select = function(covariates, responses, cutoff){
-  retained = summary(lm(responses~covariates))$coefficients[,4][summary(lm(responses~covariates))$coefficients[,4] <= cutoff]
-  indices = which(summary(lm(responses~covariates))$coefficients[,4][summary(lm(responses~covariates))$coefficients[,4] <= cutoff] %in% summary(lm(responses~covariates))$coefficients[,4])
-  again = summary(lm(responses~covariates[,indices]))$coefficients[,4][summary(lm(responses~covariates[,indices]))$coefficients[,4] <= cutoff]
-  if (length(again == 0)){
-   return(c())}
-    }.patch
+}.patch
