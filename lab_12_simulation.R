9dad01057f1cfe19096990bdd89b376da22370be
kgchung <38510598+kgchung@users.noreply.github.com>
2018-04-20 06:47:28 -0400

    wrote function that takes regression of covariates on regrression

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -3,3 +3,11 @@ generate_data = function(n,p){
   responses = rnorm(n, 0, 1)                     
   list(covariates, responses)                    
 }.patch
+
+model_select = function(covariates, responses, cutoff){
+  retained = summary(lm(responses~covariates))$coefficients[,4][summary(lm(responses~covariates))$coefficients[,4] <= cutoff]
+  indices = which(summary(lm(responses~covariates))$coefficients[,4][summary(lm(responses~covariates))$coefficients[,4] <= cutoff] %in% summary(lm(responses~covariates))$coefficients[,4])
+  again = summary(lm(responses~covariates[,indices]))$coefficients[,4][summary(lm(responses~covariates[,indices]))$coefficients[,4] <= cutoff]
+  if (length(again == 0)){
+   return(c())}
+    }.patch
